03888848687991557b53acf2dc39911d79f0931b
Kitsuya0828 <kitsuyaazuma@gmail.com>
2023-04-01 13:43:52 +0900

    feat: get author and url

--- cmd/aozora-collector/main.go
+++ cmd/aozora-collector/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -17,13 +19,48 @@ type Entry struct {
 	ZipURL   string
 }
 
-func findEntries(res *http.Response) ([]Entry, error) {
+func findAuthorAndZIP(siteURL string) (string, string) {
+	res, err := http.Get(siteURL)
+	if err != nil {
+		panic(err)
+	}
+	
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return "", ""
+	}
+
+	author := doc.Find("table[summary=作家データ] tr:nth-child(2) td:nth-child(2)").Text()
+
+	zipURL := ""
+	doc.Find("table.download a").Each(func(i int, s *goquery.Selection) {
+		href := s.AttrOr("href", "")
+		if strings.HasSuffix(href, ".zip") {
+			zipURL = href
+		}
+	})
+	return author, zipURL
+}
+
+func findEntries(siteURL string) ([]Entry, error) {
+	res, err := http.Get(siteURL)
+	if err != nil {
+		panic(err)
+	}
+
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		return nil, err
 	}
+	pat := regexp.MustCompile(`.*/cards/([0-9]+)/card([0-9]+).html$`)
 	doc.Find("ol li a").Each(func(i int, s *goquery.Selection) {
-		println(s.Text(), s.AttrOr("href", ""))
+		token := pat.FindStringSubmatch(s.AttrOr("href", ""))
+		if len(token) != 3 {
+			return
+		}
+		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html", token[1], token[2])
+		author, zipURL := findAuthorAndZIP(pageURL)
+		println(author, zipURL)
 	})
 	return nil, nil
 }
@@ -31,12 +68,7 @@ func findEntries(res *http.Response) ([]Entry, error) {
 func main() {
 	listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
 
-	res, err := http.Get(listURL)
-	if err != nil {
-		panic(err)
-	}
-
-	entries, err := findEntries(res)
+	entries, err := findEntries(listURL)
 	if err != nil {
 		log.Fatal(err)
 	}
